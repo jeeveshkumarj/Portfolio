a4daa02f435e7c368f8eca522c4671fb310dde58
jeeveshkumarj <109807668+jeeveshkumarj@users.noreply.github.com>
2025-01-01 18:24:17 +0530

    Added j

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
       <section className="p-5 bg-red-200 flex justify-center">
           <p className="font-bold">
-              ©Copyright 2025. All rights are reserved by Jeevesh Kumar 
+              ©Copyright 2025. All rights are reserved by Jeevesh Kumar J 
           </p>
       </section> 
   );
